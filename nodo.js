906b636fe705b2b73b2cfb9b2fb089730549fcec
JavierGPG <jgpastranag@correo.udistrital.edu.co>
2022-10-17 16:50:47 -0500

    Update nodo.js

--- nodo.js
+++ nodo.js
@@ -86,7 +86,6 @@ function busqueda(inicio, destino, arbol) {
                             }
                         })
                     }
-    
                 }else{
                     valor = PesoRecorrido[0];
                 }
